9a800b02f663b691fde870ac8ad69f7a1023e736
Karha <124255059+karhabernel@users.noreply.github.com>
2024-05-23 13:03:16 +0900

    0522_Initial

---
+++ Source/SurvivorGame/SurvivorGame.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SurvivorGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SurvivorGame, "SurvivorGame" );
+
+DEFINE_LOG_CATEGORY(LogSurvivorGame)
+ 
---
+++ Source/SurvivorGame/SurvivorGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SurvivorGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASurvivorGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASurvivorGameGameMode();
+};
+
+
+
